232e3265fbc937bd7cbb0f0097d8a80c481cb0e2
EC2 Default User <ec2-user@ip-172-31-38-117.ec2.internal>
2022-06-18 09:20:54 +0000

    ADD

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2022_06_18_053423) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -26,7 +26,15 @@ ActiveRecord::Schema.define(version: 2022_06_18_053423) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "password", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.string "post_code", default: "", null: false
+    t.string "aderess", default: "", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

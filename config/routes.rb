6e3ef95967beb4151bca9b298738aac01a94a169
ramusini <ramusini@gmail.com>
2022-06-19 10:09:55 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :public do
-    get 'homes/top'
+
   end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
@@ -13,21 +13,35 @@ Rails.application.routes.draw do
 
   #会員側のルーティング設定
   scope module: :public do
-
-    resources :customers, only: [:show, :edit, :update]
-    resources :items, only: [:index,:show]
+    resources :customers do
+      collection do
+        get 'unsubscribe'
+      end
+    end
+    resources :cart_items do
+      collection do
+        delete 'all_destroy'
+      end
+    end
     resources :orders do
       collection do
         get 'thanx'
         post 'confirmation'
       end
     end
-
+    resources :items, only: [:index, :show]
+    resources :shipping_addresses
+    root to: "homes#top"
+    get '/home/about' => 'homes#about', as: 'about'
   end
 
   #管理者側のルーティング設定
   namespace :admin do
-  resources :customers, only: [:index, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :oders, only: [:show, :index, :update]
+    resources :order_details, only: [:update]
+    resources :items, except: [:destry]
+    resources :item_genres, only: [:index, :edit, :update, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

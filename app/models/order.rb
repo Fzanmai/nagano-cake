f233a5cfd9e03b3ea0584146e3d7c00780f4130b
ramusini <ramusini@gmail.com>
2022-06-20 06:44:03 +0000

    [Add]画面のみ

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
-  enum :payment_method, { クレジットカード:0, 銀行振込:1 }
-  enum :production_status, { 入金待ち:0, 入金確認:1, 製作中:2, 発送準備中:3, 発送済み:4 }
+  belongs_to :customer
+
+  enum payment_method: { card: 0, furikomi: 1 }
+  enum production_status: { on_payment: 0, paid: 1, under_manufacture: 2, on_shipping: 3, shipped: 4 }
 end

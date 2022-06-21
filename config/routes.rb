d3b6a38cd732c61bb8c8dcf4297be4e78f1caf2f
EC2 Default User <ec2-user@ip-172-31-38-117.ec2.internal>
2022-06-21 08:18:40 +0000

    ADD address

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+
+  end
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -9,16 +13,38 @@ Rails.application.routes.draw do
 
   #会員側のルーティング設定
 
+  scope module: :public do
+    resources :customers do
+      collection do
+        get 'unsubscribe'
+        patch 'withdrawal'
+      end
+    end
+    resources :cart_items do
+      collection do
+        delete 'all_destroy'
+      end
+    end
+    resources :orders do
+      collection do
+        get 'thanx'
+        post 'confirmation'
+      end
+    end
+    resources :items, only: [:index, :show]
+    resources :shipping_addresses
+    root to: "homes#top"
+    get '/home/about' => 'homes#about', as: 'about'
+  end
 
   #管理者側のルーティング設定
   namespace :admin do
-
-
-
-
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :oders, only: [:show, :index, :update]
+    resources :order_details, only: [:update]
+    resources :items, except: [:destry]
+    resources :item_genres, only: [:index, :edit, :update, :create]
   end
-
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

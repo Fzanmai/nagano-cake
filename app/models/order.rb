43729d9266e6b7cf481bd5b945f764a13359792b
ramusini <ramusini@gmail.com>
2022-06-22 06:26:29 +0000

    [Start]注文詳細

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_details
 
   enum payment_method: { card: 0, furikomi: 1 }
   enum production_status: { on_payment: 0, paid: 1, under_manufacture: 2, on_shipping: 3, shipped: 4 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_051734) do
+ActiveRecord::Schema.define(version: 2022_06_22_061841) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -84,8 +84,18 @@ ActiveRecord::Schema.define(version: 2022_06_20_051734) do
     t.string "name"
     t.text "introduction"
     t.integer "price"
-    t.boolean "is_active", default: true
-    t.integer "item_genre_id"
+    t.boolean "is_active"
+    t.integer "genre_id"
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "price"
+    t.integer "quantity"
+    t.integer "order_id"
+    t.integer "order_status", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|

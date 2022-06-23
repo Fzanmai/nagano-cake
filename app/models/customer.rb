207bff83ee8162d91b63d233029eaefdf48ce9f0
EC2 Default User <ec2-user@ip-172-31-38-117.ec2.internal>
2022-06-23 04:57:03 +0000

    Add

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,14 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum is_active: {Available: true, Invalid: false}
+
   def active_for_authentication?
-    super && (is_deleted == false)
+    super && (self.is_active === "Available")
   end
   has_many :orders
   has_many :shipping_addresses, dependent: :destroy

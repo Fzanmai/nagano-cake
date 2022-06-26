e6c0793a6991cd5e9b439143ac35a130be257141
ramusini <ramusini@gmail.com>
2022-06-26 05:12:12 +0000

    [Fix]テスト仕様の内容より（未完

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    case resource
+    when Admin
+      root_path
+    when Customer
+      customer_path(current_customer.id)
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+      new_admin_session_path
+  end
+
   protected
 
   def configure_permitted_parameters
